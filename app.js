1eb3fd87726ea8e3fc80baee563afed16850e363
ArshadHakeem <arshadtechnics@gmail.com>
2023-12-07 15:51:01 +0400

    edited event varieble

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ let buttons = document.querySelectorAll('button');
 let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
+    button.addEventListener('click', (evt) =>{
+        if(evt.target.innerHTML == '='){
             string = eval(string);
             input.value = string;
         }
 
-        else if(e.target.innerHTML == 'AC'){
+        else if(evt.target.innerHTML == 'AC'){
             string = "";
             input.value = string;
         }
-        else if(e.target.innerHTML == 'DEL'){
+        else if(evt.target.innerHTML == 'DEL'){
             string = string.substring(0, string.length-1);
             input.value = string;
         }
         else{
-            string += e.target.innerHTML;
+            string += evt.target.innerHTML;
             input.value = string;
         }
         
